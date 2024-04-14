b1d417759ad64835be1b174f10ff9f617a109429
satyam-seth <satyam1998.1998@gmail.com>
2024-04-15 01:14:02 +0530

    setup static files config

--- the_split/urls.py
+++ the_split/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -25,10 +26,15 @@ admin.site.site_title = "D-52 Site Admin"
 
 # TODO: check is it right way of doing this
 media_urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+static_urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-    path("records/", include("records.urls")),
-    path("accounts/", include("accounts.urls")),
-] + media_urlpatterns
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("core.urls")),
+        path("records/", include("records.urls")),
+        path("accounts/", include("accounts.urls")),
+    ]
+    + media_urlpatterns
+    + static_urlpatterns
+)
