31dea2f58bf6194a6e93ded0b7322e86aa518fc4
satyam-seth <satyam1998.1998@gmail.com>
2024-04-21 19:38:15 +0530

    add custom manager for room invitation model to send and accept invitation methods

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.signing import Signer
+from django.db import models
 
 
 class UserManager(BaseUserManager):
@@ -36,3 +39,55 @@ class UserManager(BaseUserManager):
             raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(email, password, **extra_fields)
+
+
+class RoomInvitationManager(models.Manager):
+    """Model manager for RoomInvitation model"""
+
+    def __init__(self):
+        super().__init__()
+        # TODO: keep the salt used in production secret!
+        # self.salt = secrets.token_bytes(16).hex()
+        self.salt = "c603df19008728cab0791fab1aec6f2f"
+
+    def _get_payload_for_instance(self, invitation):
+        return {
+            "id": invitation.id,
+            "room": invitation.room.id,
+            "email": invitation.email,
+        }
+
+    def _generate_signed_token(self, invitation) -> str:
+        """Generate a signed token using the invitation and salt"""
+
+        signer = Signer(salt=self.salt)
+        payload = self._get_payload_for_instance(invitation)
+        return signer.sign_object(payload)
+
+    def _unsigned_token(self, token: str):
+        """Verify the signed token and return the invitation"""
+
+        signer = Signer(salt=self.salt)
+        return signer.unsign_object(token)
+
+    def send_invite(self, room, email: str) -> None:
+        """Create and send an invitation to join the specified room"""
+
+        invitation = self.create(room=room, email=email)
+        token = self._generate_signed_token(invitation=invitation)
+
+        # TODO: generate url and send email
+        return invitation
+
+    def accept_invitation(self, current_user, token: str) -> None:
+        """Accept the invitation for the current user using the token"""
+        payload = self._unsigned_token(token=token)
+
+        if payload.get("email") != current_user.email:
+            raise ValidationError("Invitation token is not for the current user")
+
+        try:
+            invitation = self.model.objects.get(id=payload.get("id"))
+            invitation.accept()
+        except self.model.DoesNotExist as e:
+            raise ValidationError("Invitation not found") from e
