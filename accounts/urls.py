dea30e0a2c8d0c25866b669bff96885bb4e13fa4
satyam-seth <satyam1998.1998@gmail.com>
2024-04-28 20:21:59 +0530

    Add room selection page

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
     path("signup/", views.UserSignUpView.as_view(), name="signup"),
     path("room/", views.RoomTemplateView.as_view(), name="room"),
+    path("room_selection/", views.RoomSelectionView.as_view(), name="room_selection"),
     # path("room_join/", views.RoomJoinView.as_view(), name="room_join"),
     path("room_create/", views.RoomCreateView.as_view(), name="room_create"),
     path(
