1f979bb71076f3863e90ae029284abb7c468f0a7
satyam-seth <satyam1998.1998@gmail.com>
2024-05-02 00:48:32 +0530

    Add room admin required mixin

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -1,5 +1,9 @@
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import AccessMixin, UserPassesTestMixin
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+
+from accounts.models import Room
 
 
 class RoomSessionMixin:
@@ -13,3 +17,22 @@ class RoomSessionMixin:
             return redirect(reverse_lazy("accounts:room_selection"))
 
         return super().dispatch(request, *args, **kwargs)
+
+
+class RoomAdminRequiredMixin:
+    """
+    Mixin to check if the current user is the admin of
+    the room corresponding to the session room id.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        If current user is not admin of room corresponding
+        to the session room id, return forbidden response.
+        """
+
+        room_id = request.session["room_id"]
+        room = get_object_or_404(Room, pk=room_id)
+        if room.admin == request.user:
+            return super().dispatch(request, *args, **kwargs)
+        return HttpResponseForbidden()
