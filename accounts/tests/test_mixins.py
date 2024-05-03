b95c0255c6a8faa2cf148bfcda17042ce3efb92a
satyam-seth <satyam1998.1998@gmail.com>
2024-05-03 22:21:57 +0530

    Add unit test for room admin required mixin

--- accounts/tests/test_mixins.py
+++ accounts/tests/test_mixins.py
@@ -1,9 +1,13 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import RequestFactory, TestCase
 from django.urls import reverse_lazy
 
-from accounts.mixins import RoomSessionMixin
+from accounts.mixins import RoomAdminRequiredMixin, RoomSessionMixin
+from accounts.models import Room
+
+User = get_user_model()
 
 
 class TestRoomRequiredMixin(TestCase):
@@ -47,3 +51,50 @@ class TestRoomRequiredMixin(TestCase):
         ):
             # Call the dispatch method with the request
             view.dispatch(request)
+
+
+class TestRoomAdminRequiredMixin(TestCase):
+    """Test Room Admin Required Mixin"""
+
+    def setUp(self) -> None:
+        self.admin = User.objects.create_user(
+            email="admin@user.com", password="test-password"
+        )
+        self.room = Room.objects.create(name="Test Room", admin=self.admin)
+        self.other_user = User.objects.create_user(
+            email="member@user.com", password="test-password"
+        )
+
+        self.factory = RequestFactory()
+        self.request = self.factory.get("/")
+
+        # Set room id in session
+        self.request.session = {"room_id": self.room.id}
+
+        # Create a view instance with the RoomAdminRequiredMixin
+        self.view = RoomAdminRequiredMixin()
+
+    def test_user_is_admin(self):
+        """Test user is admin"""
+
+        # set admin as request user
+        self.request.user = self.admin
+
+        # Assert calling dispatch should call super dispatch
+        with self.assertRaisesMessage(
+            AttributeError, "'super' object has no attribute 'dispatch'"
+        ):
+            # Call the dispatch method with the request
+            self.view.dispatch(self.request)
+
+    def test_user_is_not_admin(self):
+        """Test user is not admin"""
+
+        # set other user as request user
+        self.request.user = self.other_user
+
+        # Call the dispatch method with the request
+        response = self.view.dispatch(self.request)
+
+        # Assert access should be denied with Forbidden status code
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
