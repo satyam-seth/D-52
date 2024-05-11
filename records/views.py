6a11b0f71bc277a26df021429b88e9063560b5ce
satyam-seth <satyam1998.1998@gmail.com>
2024-05-11 20:15:05 +0530

    fix user record list view missing ordering and fix get_queryset method

--- records/views.py
+++ records/views.py
@@ -104,14 +104,13 @@ class UserRecordListView(ListView):
     model = Record
     paginate_by = 20
     paginate_orphans = 10
+    ordering = ["-purchase_date"]
 
     # TODO: Add return type once this issue is fixed
     # - https://github.com/typeddjango/django-stubs/issues/477
     # def get_queryset(self) -> QuerySet[Any]:
     def get_queryset(self):
-        queryset = Record.objects.filter(purchaser__id=self.kwargs["user_id"]).order_by(
-            "-purchase_date"
-        )
+        queryset = super().get_queryset().filter(purchaser__id=self.kwargs["user_id"])
         return queryset
 
 
