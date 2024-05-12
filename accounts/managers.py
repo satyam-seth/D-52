39cac9e7f403e8e7df9c286ff1630a358e20bbce
satyam-seth <satyam1998.1998@gmail.com>
2024-05-12 15:25:02 +0530

    fix missing first_name and last_name args in create-super_user User mdoel manager method

--- accounts/managers.py
+++ accounts/managers.py
@@ -55,7 +55,14 @@ class UserManager(BaseUserManager):
             **extra_fields,
         )
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(
+        self,
+        email: str,
+        password: str,
+        first_name: str,
+        last_name: str,
+        **extra_fields: Any,
+    ):
         """Create and save a SuperUser with the given email and password."""
 
         extra_fields.setdefault("is_staff", True)
@@ -66,7 +73,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
 
 
 class RoomInvitationManager(models.Manager):
