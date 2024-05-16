0b517d82ed8408205711218e9d9df1b79aa0b730
satyam-seth <satyam1998.1998@gmail.com>
2024-05-16 21:15:24 +0530

    add unit test for room_invitation_reject url

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -62,6 +62,12 @@ class UrlsTestCase(TestCase):
         url = reverse("accounts:room_invitation_accept")
         self.assertEqual(resolve(url).func.view_class, views.RoomInvitationAcceptView)
 
+    def test_room_invitation_reject_url(self):
+        """Test room_invitation_reject url resolve"""
+
+        url = reverse("accounts:room_invitation_reject")
+        self.assertEqual(resolve(url).func.view_class, views.RoomInvitationRejectView)
+
     def test_room_create_url(self):
         """Test room_create url resolve"""
 
