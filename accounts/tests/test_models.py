83613509aff2a6c85017a8afb55d111076e4a76b
satyam-seth <satyam1998.1998@gmail.com>
2024-05-26 16:06:08 +0530

    add unit test for user model str method

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -9,7 +9,39 @@ from accounts.models import Profile, Room, RoomInvitation, RoomMembership
 User = get_user_model()
 
 
-class TestProfileModel(TestCase):
+class TestUserModel(TestCase):
+    """Test User Model"""
+
+    def test_str_method_for_user_with_full_name(self) -> None:
+        """Test str method for user with full name"""
+
+        # Create user
+        user = User.objects.create_user(
+            email="test@user.com",
+            password="test-password",
+            first_name="test",
+            last_name="user",
+        )
+
+        # Assert that the string representation of the user instance is the user's full name
+        self.assertEqual(str(user), user.get_full_name())
+
+    def test_str_method_for_user_without_full_name(self) -> None:
+        """Test str method for user without full name"""
+
+        # Create user
+        user = User.objects.create_user(
+            email="test@user.com",
+            password="test-password",
+            first_name="",
+            last_name="",
+        )
+
+        # Assert that the string representation of the user instance is the user's email
+        self.assertEqual(str(user), user.email)
+
+
+class TestProfileModelTest(TestCase):
     """Test Profile Model"""
 
     def setUp(self) -> None:
