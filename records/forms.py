d7479da8a468a1a8a5e70effd653b1e7814edd26
satyam-seth <satyam1998.1998@gmail.com>
2024-06-04 00:32:34 +0530

    add mix and max attributes for water form quantity field widget

--- records/forms.py
+++ records/forms.py
@@ -65,5 +65,13 @@ class WaterFrom(forms.ModelForm):
                     "value": localtime(now()).date(),
                 }
             ),
-            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(
+                # TODO: fix it min value still 0 in html input tag
+                # and it can be inhabited from model field validator
+                attrs={
+                    "class": "form-control",
+                    "min": 1,
+                    "max": 5,
+                }
+            ),
         }
