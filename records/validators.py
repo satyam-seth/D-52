b2331396df983b64dbc57eb0e98c33aab919e511
Satyam Seth <satyam1998.1998@gmail.com>
2025-02-16 18:58:47 +0530

    Add validator for past datetime check

--- records/validators.py
+++ records/validators.py
@@ -1,9 +1,29 @@
+from datetime import datetime, timedelta
 from functools import partial
 
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 
+def validate_past_datetime(value: datetime, days: int) -> None:
+    """
+    Validator function to ensure that the datetime is not in the future and is
+    within the past 'n' number of days
+    """
+
+    now = timezone.now()
+    n_days_ago = now - timedelta(days=days)
+
+    # Check if the value is in the future
+    if value > now:
+        raise ValidationError("The date cannot be in the future.")
+
+    # Check if the value is more than n days ago
+    if value < n_days_ago:
+        raise ValidationError(f"The date must be within the last {days} days.")
+
+
+# TODO: Remove it
 def validate_past_date(value, n_days):
     """
     Validator function to ensure that the date is at least n days in the past
