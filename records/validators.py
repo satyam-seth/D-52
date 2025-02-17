66b790139f488bf8f0892366c35b005f8d22d856
Satyam Seth <satyam1998.1998@gmail.com>
2025-02-18 01:22:35 +0530

    Remove validate_past_date and validate_past_date_within_past_6_days validators

--- records/validators.py
+++ records/validators.py
@@ -23,23 +23,4 @@ def validate_past_datetime(value: datetime, days: int) -> None:
         raise ValidationError(f"The date and time must be within the last {days} days.")
 
 
-# TODO: Remove it
-def validate_past_date(value, n_days):
-    """
-    Validator function to ensure that the date is at least n days in the past
-    and disallow future dates.
-    """
-
-    today = timezone.now().date()
-    if value > today:
-        raise ValidationError("Future dates are not allowed.")
-
-    min_date = today - timezone.timedelta(days=6)
-    if value < min_date:
-        raise ValidationError(f"Date should be at least {n_days} days in the past.")
-
-
-# TODO: Remove the hard-coded 6-day value and get it from a setting or environment variable
-validate_past_date_within_past_6_days = partial(validate_past_date, n_days=6)
-
 validate_past_datetime_within_past_6_days = partial(validate_past_datetime, days=6)
