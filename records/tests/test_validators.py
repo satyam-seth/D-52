d5bb6612d94ccbc7997ad5bf9314cd1f465fb5c6
Satyam Seth <satyam1998.1998@gmail.com>
2025-02-18 01:50:05 +0530

    Add unit test for validate past datetime validator

--- records/tests/test_validators.py
+++ records/tests/test_validators.py
@@ -2,44 +2,43 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils import timezone
 
-from records.validators import validate_past_date
+from records.validators import validate_past_datetime
 
 
-class TestValidatePastDate(TestCase):
-    """Test validate past date validator"""
+class TestValidatePastDateTime(TestCase):
+    """Test validate past datetime validator"""
 
-    def test_valid_past_date(self):
-        """test valid past date"""
+    def test_valid_past_datetime(self):
+        """test valid past datetime"""
 
-        # Test with a date that is today
-        today = timezone.now().date()
-        self.assertIsNone(validate_past_date(today, n_days=6))
+        # Test with a datetime that is now
+        now = timezone.now()
+        self.assertIsNone(validate_past_datetime(now, days=6))
 
-        # Test with a date that is within 6 days in the past
-        past_date = today - timezone.timedelta(days=6)
-        self.assertIsNone(validate_past_date(past_date, n_days=6))
+        # Test with a datetime that is within n days in the past
+        past_date = now - timezone.timedelta(days=4)
+        self.assertIsNone(validate_past_datetime(past_date, days=6))
 
-    def test_future_date(self):
-        """Test future date"""
+    def test_future_datetime(self):
+        """Test future datetime"""
 
-        # Test with a future date
-        future_date = timezone.now().date() + timezone.timedelta(days=1)
-
-        with self.assertRaisesMessage(
-            ValidationError,
-            "Future dates are not allowed.",
-        ):
-            validate_past_date(future_date, n_days=6)
-
-    def test_date_too_far_in_past(self):
-        """Test date too far in fast"""
-
-        # Test with a date that is more than 6 days in the past
-        today = timezone.now().date()
-        past_date = today - timezone.timedelta(days=7)
+        # Test with a future datetime
+        future_datetime = timezone.now() + timezone.timedelta(days=1)
 
         with self.assertRaisesMessage(
             ValidationError,
-            "Date should be at least 6 days in the past.",
+            "The date and time cannot be in the future.",
         ):
-            validate_past_date(past_date, n_days=6)
+            validate_past_datetime(future_datetime, days=6)
+
+    def test_datetime_too_far_in_past(self):
+        """Test datetime too far in fast"""
+
+        # Test with a datetime that is more than n days in the past
+        past_date = timezone.now() - timezone.timedelta(days=7)
+
+        with self.assertRaisesMessage(
+            ValidationError,
+            "The date and time must be within the last 6 days.",
+        ):
+            validate_past_datetime(past_date, days=6)
