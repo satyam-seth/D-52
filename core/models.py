02b19d5324007155841ac6e032a2e6ee5fcfc91c
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-05 01:27:21 +0530

    Add BaseCreatedModifiedModel

--- core/models.py
+++ core/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class BaseCreatedModifiedModel(models.Model):
+    """
+    Abstract base model that provides self-updating
+    created_on and modified_on timestamp fields
+    """
+
+    modified_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
 class Feedback(models.Model):
     """Model to store feedback from users"""
 
