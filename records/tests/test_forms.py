f8de4a7696abb73d0813bd8d540b0cfc52c614f6
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-12 23:59:32 +0530

    Fix RecordForm.test_record_form_fields unit test

--- records/tests/test_forms.py
+++ records/tests/test_forms.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
-from django.utils.timezone import localtime, make_aware, now
+from django.utils.timezone import localtime, make_aware
 
 from accounts.models import Room
 from records.forms import RecordForm, WaterForm
@@ -35,10 +35,16 @@ class TestRecordForm(TestCase):
     def test_record_form_fields(self, mock_now):
         """Test record form fields"""
 
+        now = make_aware(datetime(2025, 2, 16, 10, 30))
+
         # set mock now return value
-        mock_now.return_value = make_aware(datetime(2025, 2, 16, 10, 30))
+        mock_now.return_value = now
 
         form = RecordForm()
+
+        # assert attributes
+        self.assertEqual(form.now, now)
+        self.assertIsNone(form.room_id)
 
         # assert meta class
         self.assertEqual(form.Meta.model, Record)
@@ -58,18 +64,18 @@ class TestRecordForm(TestCase):
             "form-control",
         )
         self.assertEqual(
-            form.Meta.widgets["purchase_datetime"].attrs["min"],
-            localtime(
-                now() - timedelta(days=form.Meta.model.max_allowed_past_days)
-            ).strftime("%Y-%m-%dT%H:%M"),
+            form.fields["purchase_datetime"].widget.attrs["min"],
+            (localtime(now) - timedelta(days=Record.max_allowed_past_days)).strftime(
+                "%Y-%m-%dT%H:%M"
+            ),
         )
         self.assertEqual(
-            form.Meta.widgets["purchase_datetime"].attrs["max"],
-            localtime(now()).strftime("%Y-%m-%dT%H:%M"),
+            form.fields["purchase_datetime"].widget.attrs["max"],
+            localtime(now).strftime("%Y-%m-%dT%H:%M"),
         )
         self.assertEqual(
-            form.Meta.widgets["purchase_datetime"].attrs["value"],
-            localtime(now()).strftime("%Y-%m-%dT%H:%M"),
+            form.fields["purchase_datetime"].widget.attrs["value"],
+            localtime(now).strftime("%Y-%m-%dT%H:%M"),
         )
 
         # assert purchase_date purchaser
