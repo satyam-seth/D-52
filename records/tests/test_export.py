6f526a950707c5b3856c5b518bc21ce624c9223c
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-30 01:35:03 +0530

    Write unit test for RoomExporter class get_electricity_df method

--- records/tests/test_export.py
+++ records/tests/test_export.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from unittest.mock import MagicMock, patch
 
+import pandas as pd
 from django.test import TestCase
 from django.utils.timezone import make_aware
 
 from records.export import RoomExporter
+from records.models import Electricity
 
 
 class RoomExporterTest(TestCase):
@@ -31,3 +34,20 @@ class RoomExporterTest(TestCase):
         test_time = make_aware(datetime(2025, 2, 18, 14, 30))
         formatted_time = self.exporter.get_formatted_time(test_time)
         self.assertEqual(formatted_time, "02:30:00 PM")
+
+    @patch("records.export.RoomExporter.get_entry_df")
+    def test_get_electricity_df(self, mock_get_entry_df: MagicMock):
+        """Mock test for get electricity df"""
+
+        # Prepare mock DataFrame
+        mock_df = pd.DataFrame([{"Mock": "Data"}])
+        mock_get_entry_df.return_value = mock_df
+
+        # Call the method you're testing
+        df = self.exporter.get_electricity_df()
+
+        # Assert that get_entry_df was called with Electricity as an argument
+        mock_get_entry_df.assert_called_with(Electricity)
+
+        # Assert the DataFrame returned is as expected
+        self.assertTrue(df.equals(mock_df))
